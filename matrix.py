2826530635411d5924cb10d271c4c2cf50239996
netaamz <netaamz@gmail.com>
2024-03-11 12:34:21 +0200

    rackett added and optimaizetion

--- matrix.py
+++ matrix.py
@@ -44,9 +44,70 @@ def gaussian_elimination(m):
             row+=1
         col+=1
       
-       
-    return (res,scalar,flag,inverted)    
+        
+    return (res,scalar,flag,inverted)   
+def gaussian_elimination1(m):
+    """
+    Function to preform the gaussian elimination of a matrix.
+
+    Parameters:
+    Input matrix m of shape (n, n).
+
+    Returns:
+    Gaussian elimination of the input matrix.
     
+    """
+    row, col = 0, 0
+    n = m.shape[0]
+    res = np.copy(m).astype(float)
+    flag=1
+    scalar=1.0
+    inverted= np.identity(n)
+    while row < n and col < n:
+        print("row:",row)
+        pivot_row = find_pivot_row(res, row, col)
+        if pivot_row!=None :
+            # swap the rows
+            if pivot_row !=row:
+                inverted[row]+=res[pivot_row]
+                res[row]+=res[pivot_row]
+            if row<n:
+                vec_col=res[row+1:,col].reshape(-1,1)
+                print("vec_col")
+                print(vec_col)
+              #  vec_col=vec_col[row:]
+                vec_col/=res[row,col]
+                vec_row=res[row].reshape(1,-1)
+                print("vec_row")
+                print(vec_row)
+                mat_multiply=vec_col*vec_row
+                print("mat_multiply")
+                print(mat_multiply)
+                print("res[row+1:,:]")
+                print(res[row+1:,:])
+                inverted[row+1:,:]-=mat_multiply
+                res[row+1:,:]-=mat_multiply
+                
+            # divide the rows elements with the first nonzero element 
+            scalar /=res[row][col]
+            # inverted[row] /= res[row][col]
+            # res[row] /= res[row][col]
+            if pivot_row != row: 
+                flag *=-1
+            
+            # Reset all other elements in the current column
+            #for r in range(n):
+                #if r!= pivot_row:
+                    #factor = res[r, col]
+                    #inverted[r] -= factor * inverted[pivot_row]
+                   # res[r] -= factor * res[pivot_row]
+                    
+            row+=1
+        col+=1
+      
+        
+    return (res,scalar,flag,inverted)  
+
 def find_pivot_row(matrix, start_row, col):
     """
     Function to find the pivot row in a given column of the matrix.
@@ -126,7 +187,7 @@ def rank(m):
 
 def matrix_method(m):
     
-    echelon_form,scalar,flag,inver=gaussian_elimination(m)
+    echelon_form,scalar,flag,inver=gaussian_elimination1(m)
     det=determinant(echelon_form, scalar,flag)
     rank_m=rank(echelon_form)
     print("\n\MY METHODS: ")
@@ -148,7 +209,10 @@ def pymethods(matrix):
     if det!=0:
         print("Invert of the matrix:\n", inv)
 print("1.det_matrix(800 x 800).txt")       
-matrix= np.loadtxt('det_matrix(800 x 800).txt', usecols=range(800))
+#matrix= np.loadtxt('det_matrix(800 x 800).txt', usecols=range(800))
+
+matrix= np.array([[1, 2, 3],
+                   [4, 5, 6],[7,2,9]]) 
 t=time()
 matrix_method(matrix)
 print("TIME:",time()-t)
@@ -159,27 +223,27 @@ pymethods(matrix)
 print("TIME:",time()-t)
 
 
-print("2.inv_eig_matrix(800 x 800).txt")      
-matrix= np.loadtxt('inv_eig_matrix(800 x 800).txt', usecols=range(800))
-t=time()
-matrix_method(matrix)
-print("TIME:",time()-t)
+# print("2.inv_eig_matrix(800 x 800).txt")      
+# matrix= np.loadtxt('inv_eig_matrix(800 x 800).txt', usecols=range(800))
+# t=time()
+# matrix_method(matrix)
+# print("TIME:",time()-t)
 
 
-t=time()
-pymethods(matrix)
-print("TIME:",time()-t)
+# t=time()
+# pymethods(matrix)
+# print("TIME:",time()-t)
 
-print("3.rank_matrix(1000x1000).txt")
-matrix = np.loadtxt('rank_matrix(1000x1000).txt', delimiter=',', usecols=range(1000))
-
-
-t=time()
-matrix_method(matrix)
-print("TIME:",time()-t)
+# print("3.rank_matrix(1000x1000).txt")
+# matrix = np.loadtxt('rank_matrix(1000x1000).txt', delimiter=',', usecols=range(1000))
 
 
-t=time()
-pymethods(matrix)
-print("TIME:",time()-t)
+# t=time()
+# matrix_method(matrix)
+# print("TIME:",time()-t)
+
+
+# t=time()
+# pymethods(matrix)
+# print("TIME:",time()-t)
 
