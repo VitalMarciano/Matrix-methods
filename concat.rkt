e308b27e52410384e33e492305d764f96e212e45
netaamz <netaamz@gmail.com>
2024-03-13 18:26:01 +0200

    added find_pivot, gauusien elimination
    for gauusien elimination
    יש שילדה לתנאים של הרקורסיה שיהיה קל  לסדר
    להוסיף פונקציה שעושה את כל החישובים על המטריצה ולשלוח בקריאה רקורסיבית את המטריציה בתוכה

--- concat.rkt
+++ concat.rkt
@@ -1,23 +1,105 @@
 #lang racket
 (require flomat)
 
+(define swap-rows(λ(mat rowi pivot-row n)
+                       
+                    (let((tmp1 (sub mat rowi 0 1 n)) (tmp2 (sub mat pivot-row 0 1 n)))
+                         (begin
+                         (insert_row mat rowi tmp2 n)
+                         (insert_row mat pivot-row tmp1 n)
+                         )
+                     )))
 
-(define A (matrix '((1 2 3) (11 22 33)
-                    (33 44 55))))
+(define A (matrix '((0 0 3) (1 11 22)(0 44 22))))
+(for/list ([   x  (in-row A 0)]) x)
+
 
 (define insert_row (λ(mat i rowi n)
-      (let ((sub-top (sub mat 0 0 i n)))
-        (let ((sub-buttom (sub mat (+ i 1) 0 (-(- n i)1) n)))
-           (stack sub-top (stack rowi sub-buttom))
-        ))))
+           (cond
+             ((= i 0) (stack rowi (sub mat (+ i 1) 0 (-(- n i)1) n)))
+             ((= i (- n 1))(stack (sub mat 0 0 i n) rowi))
+             (else (let((sub-top (sub mat 0 0 i n))
+                        (sub-buttom (sub mat (+ i 1) 0 (-(- n i)1) n)))
+                     (stack sub-top (stack rowi sub-buttom))))
+                  )))
+
+(define insert_col (λ(mat i coli n)
+           (cond
+             ((= i 0) (augment  coli (sub mat  0 (+ i 1) n (-(- n i)1))))
+             ((= i (- n 1))(augment  (sub mat 0 0 n i) coli))
+             (else (let((sub-top (sub mat 0 0 n i))
+                        (sub-buttom (sub mat  0 (+ i 1) n (-(- n i)1))))
+                     (augment  sub-top (augment  coli sub-buttom))))
+                  )))
+
+
+
+(define find-pivot(λ (mat start-row coli n)
+                    (cond 
+                          ((= start-row n) -1)
+                          ((not (= (ref mat start-row coli) 0))
+                          start-row)
+                     (else (find-pivot mat (+ start-row 1) coli n)))))
+
+(define gaussian-elimination (λ (mat i j n)
+          ;1.stop recurstion
+          ;(or (= i n) (= j n) (mat))
+          ;2. no pivot col+=1
+          ; ((= pivot -1 ) (gaussian-elimination mat i (+ j 1) n) )
+          ;3.  row+=1 (defualt after all the operations sends with the new martix )
+          ; ((= pivot -1 ) (gaussian-elimination mat (+ i 1) j n) )
+
+
+                               
+                               ;(cond (
+
+                                      ;stop recurstion
+
+                                   ;   (or (= i n) (= j n) (mat))
+                              (begin
+                               ; working
+                               (define pivot (find-pivot mat i j n))
+   
+                                        ; pivot != row aka i
+                                        (and (positive? pivot) (not(= i pivot))
+                                             ;sum res[row]+=res[pivot_row]
+                                            (insert_row mat i (.+ (sub mat i 0 1 n)(sub mat pivot 0 1 n)) n))
+                                             ;add a function that substruct and do the multi matrix
+                                             ;call her here
+                              ;not working
+                              ;   (let*
+                              ;   (coli (./ (sub mat 0 j n 1) (ref mat i j)))
+                              ;   (rowi (sub mat i 0 1 n))
+                              ;   (multi-mat (outer coli rowi))
+                              ;     )
+                                        ; no pivot col+=1
+                                        ;((= pivot -1 ) (gaussian-elimination mat i (+ j 1) n) )
+                                        
+
+                               )))
 (define determinente (λ(mat det a i n)
-                       ((= i n) (* det  a))
-                       (determinente(mat (* det (ref mat i i) a (+ i 1) n)))))
-(define c (transpose(matrix '[33 44 55])))
-(define x (insert_row A 1 c 3))
+                       (if (= i n) (* det  a)
+                       (determinente mat (* det (ref mat i i)) a (+ i 1) n))))
+
+
+(define rank (λ(mat n i r)
+               (cond ((= i n) r)
+                   ((not(= (ref mat i i) 0))(rank mat n (+ i 1) (+ r 1))))))
+
+
+(display A)
+(define c (transpose(matrix '[0 44 78])))
+(define v (matrix '[0 44 78]))
+(define x (gaussian-elimination A 0 0 3))
 (display x)
 (define d (determinente A 1 -1 0 3))
 (display d)
+
+(define colA (sub A 0 0 3 1))
+(define rowA (sub A 0 0 1 3))
+
+(display "\n")
+(display (outer colA rowA))
 (define path "det_matrix(800 x 800).txt")
 (define delim #rx"([ ]*(,)[ ]*)|([ ]+)")
 
