b22074670d805b0a6c03caaac4bf7a0191919b8f
Vital Marciano <74133733+VitalMarciano@users.noreply.github.com>
2024-03-15 15:42:36 +0200

    Update matrix_rank_det_invers_gauss_jordan_elimination.py

--- matrix_rank_det_invers_gauss_jordan_elimination.py
+++ matrix_rank_det_invers_gauss_jordan_elimination.py
@@ -18,19 +18,22 @@ class Matrix:
         matrix (numpy.ndarray): Input matrix of shape (n, n).
         """
         self.matrix = matrix
-        self.echelon_form, self.scalar, self.invereted_matrix = self.gaussian_elimination()
+        self.rank=0
 
-    def gaussian_elimination(self):
+
+    def inverse_matrix(self):
         """
-        Function to perform Gaussian elimination of a matrix.
+        Function to calculate the inverse of a matrix.
+
+        Parameters:
+        Input matrix m of shape (n, n).
 
         Returns:
-        Gaussian elimination of the input matrix.
+        Inverse of the input matrix.
         """
         row, col = 0, 0
         n = (self.matrix).shape[0]
         res = np.copy(self.matrix).astype(float)
-        scalar=1.0
         inverted= np.identity(n)
         while row < n and col < n:
             
@@ -57,25 +60,48 @@ class Matrix:
                 res-=mat_multiply
     
                 # divide the rows elements with the first nonzero element 
-                scalar /=res[row][col]
                 inverted[row] /= res[row][col]
                 res[row] /= res[row][col]
                
-    
-                # Reset all other elements in the current column
-                # for r in  [ 0.       range(n):
-                #       if r!= pivot_row:
-                #           factor = res[r, col]
-                #           inverted[r] -= factor * inverted[pivot_row]
-                #           res[r] -= factor * res[pivot_row]
                         
                 row+=1
             col+=1
            
-          
-        print(res)
-        return (res,scalar,inverted)  
+    
+        return inverted
+  
+    
+    def gaussian_elimination(self):
+        """
+        Function to perform Gaussian elimination of a matrix.
 
+        Returns:
+        Gaussian elimination of the input matrix.
+        """
+        row, col = 0, 0
+        n = (self.matrix).shape[0]
+        res = np.copy(self.matrix).astype(float)
+        pivot_row=0
+        while pivot_row < n and col < n:
+            pivot_row = self.find_pivot_row(res, row, col)
+            if pivot_row!=None :
+                # swap the rows
+                self.rank+=1
+                if pivot_row !=row :
+                    res[row]+=res[pivot_row]
+                     
+                vec_col=np.copy(res[row+1:,col].reshape(-1,1))
+                vec_col=vec_col/res[row,col]
+                vec_row=np.copy(res[row]).reshape(1,-1)
+                mat_multiply=vec_row*vec_col
+                res[row+1:,:]-=mat_multiply 
+                
+                row+=1
+            col+=1
+        
+        return res 
+    
+    
     def find_pivot_row(self, matrix, start_row, col):
         """
         Function to find the pivot row in a given column of the matrix.
@@ -96,55 +122,51 @@ class Matrix:
         Returns:
         Determinant of the input matrix.
         """
-        m = self.echelon_form
-
-        n = m.shape[0]
-        det = 1
-        for i in range(n):
-            det *= m[i][i]
-            if det == 0:
-                return det
-        if det != 0:
-            det /= self.scalar
+        m = self.gaussian_elimination()
+        det= np.prod(np.diag(m))
         return det
 
-    def rank(self):
+    def mat_rank(self):
         """
         Function to calculate the rank of a matrix.
 
         Returns:
         Rank of the input matrix.
         """
+        self.gaussian_elimination()
+        return self.rank
 
-        rank_m = 0
-        m = self.echelon_form
-        n = m.shape[0]
-        for row in range(n):
-            for col in range(row, n):
-                if m[row][col] != 0:
-                    rank_m += 1
-                    break
-        return rank_m
+    
+      
 
-    def inverse_matrix(self):
-        """
-        Function to calculate the inverse of a matrix.
-
-        Parameters:
-        Input matrix m of shape (n, n).
-
-        Returns:
-        Inverse of the input matrix.
-        """
-        #if self.determinant() != 0:
-        return self.invereted_matrix
-        #return None
-matrix=Matrix( np.array([[0, 3],[1,11]]))
-#matrix=np.loadtxt('det_matrix(800 x 800).txt', usecols=range(800))
+print("det_matrix(800 x 800).txt") 
+matrix=np.loadtxt('det_matrix(800 x 800).txt', usecols=range(800))
+matrix= Matrix(matrix)
 t=time()
-#matrix= Matrix(matrix)
-# print(matrix.rank()) 
 print(matrix.determinant()) 
+print("TIME:",time()-t) 
+
+print("python")
+t=time()
+print(np.linalg.det(matrix.matrix))
+print("TIME:",time()-t) 
+
+print("\nrank_matrix(1000x1000).txt")
+matrix = np.loadtxt('rank_matrix(1000x1000).txt', delimiter=',', usecols=range(1000))
+matrix= Matrix(matrix)
+t=time()
+print(matrix.mat_rank()) 
+print("TIME:",time()-t) 
+
+print("python")
+t=time()
+print(np.linalg.matrix_rank(matrix.matrix))
+print("TIME:",time()-t)
+
+print("\ninv_eig_matrix(800 x 800).txt")      
+matrix= np.loadtxt('inv_eig_matrix(800 x 800).txt', usecols=range(800))
+matrix= Matrix(matrix)
+t=time()
 print(matrix.inverse_matrix())
 print("TIME:",time()-t) 
 
