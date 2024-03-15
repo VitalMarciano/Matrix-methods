f33d3b4e72ae850538755974f876eeb54c45bacd
netaamz <netaamz@gmail.com>
2024-03-15 18:04:10 +0200

    added documentation

--- concat.rkt
+++ concat.rkt
@@ -2,147 +2,108 @@
 (require flomat)
 
 
-(define insert-row (λ(mat i rowi n)
+(define insert-row (λ(Mat i rowi n)
+                     ; Insert a row into a matrix at a specified index
            (cond
-             ((= i 0) (stack rowi (sub mat (+ i 1) 0 (-(- n i)1) n)))
-             ((= i (- n 1))(stack (sub mat 0 0 i n) rowi))
-             (else (let((sub-top (sub mat 0 0 i n))
-                        (sub-buttom (sub mat (+ i 1) 0 (-(- n i)1) n)))
+             ((= i 0) (stack rowi (sub Mat (+ i 1) 0 (-(- n i)1) n)))
+             ((= i (- n 1))(stack (sub Mat 0 0 i n) rowi))
+             (else (let((sub-top (sub Mat 0 0 i n))
+                        (sub-buttom (sub Mat (+ i 1) 0 (-(- n i)1) n)))
                      (stack sub-top (stack rowi sub-buttom))))
                   )))
 
-(define eliminate (λ(mat i j col-mat pivot n)
-                    " get mat, i (row index), j (col index) and n
-                      return row i * (col j)/ matrix[i][j]"
+(define eliminate (λ(Mat i j col-mat pivot n)
+                    ; Perform row elimination operation to create zeros above and below the pivot element
                 (let*                    
                     (
-                     (coli (mset! ( ./ col-mat pivot ) i 0 0))     
-                     (rowi (sub mat i 0 1 n))
-                     (mult-mat (outer coli rowi))
-                     (res-mat (.- mat mult-mat))
-                  
+                     (V_col (mset! ( ./ col-mat pivot ) i 0 0))     
+                     (V_row (sub Mat i 0 1 n))
+                     (M_mult (outer V_col V_row))
+                     (M_res (.- Mat M_mult))
                      )
-                  
-                    (insert-row res-mat i (./ rowi pivot) n))
+                    (insert-row M_res i (./ V_row pivot) n));divide by the pivot element
                   ))
-(define eliminate-rows (λ(mat i j pivot n)
-                    " get mat, i (row index), j (col index) and n
-                      return row i * (col j)/ matrix[i][j]"
+(define eliminate-rows (λ(Mat i j pivot n)
+                         ; Perform row elimination operation to create zeros below the pivot element
                 (let*                    
                     (
-                     (coli ( ./ (sub mat 0 j n 1) pivot ))
-                     (col-s (sub! coli 0 0 ( + i 1) 1))
-                     (rowi (sub mat i 0 1 n))
+                     (V_col ( ./ (sub Mat 0 j n 1) pivot ))
+                     (col-s (sub! V_col 0 0 ( + i 1) 1))
+                     (V_row (sub Mat i 0 1 n))
                      )
                   (zeros! col-s)
-                  (.- mat  (outer coli rowi)) 
+                  (.- Mat  (outer V_col V_row)) 
                   )))
 
 
-(define find-pivot(λ (mat start-row coli n)
+(define find-pivot(λ (Mat start-row col n)
+                     ; Find the pivot element in the matrix for Gaussian elimination
                     (cond
-
-                      ((= start-row n) -1)
-                      ((not (= (ref mat start-row coli) 0))
+                      ((= start-row n) -1); If we've reached the end of the matrix, return -1
+                      ((not (= (ref Mat start-row col) 0)); If element is not zero, it's a pivot
                           start-row)
-                     (else (find-pivot mat (+ start-row 1) coli n)))))
+                     (else (find-pivot Mat (+ start-row 1) col n))))); Otherwise, search in the next row
 
-(define inverse-matrix-calculation (λ (mat invers i j n)
-          "1.stop recurstion
-          (or (= i n) (= j n) (mat))
-          2. no pivot col+=1
-           ((= pivot -1 ) (gaussian-elimination mat i (+ j 1) n) )
-          3.  row+=1 (defualt after all the operations sends with the new martix )
-           ((= pivot -1 ) (gaussian-elimination mat (+ i 1) (+ j 1) n) )"
-                               ;stop recurstion
-                                 (if (or (= i n) (= j n)) invers
-                                  (let* ((pivot (find-pivot mat i j n)))
-                                    ; pivot != row aka i: sum res[row]+=res[pivot_row]
+(define inverse-matrix-calculation (λ (Mat Inverted_mat i j n)
+                                     ; Calculate the inverse of a matrix using Gaussian elimination
+                              
+                                 (if (or (= i n) (= j n)) Inverted_mat ; Base case: stop recursion when we've traversed the entire matrix
+                                  (let* ((pivot (find-pivot Mat i j n)))
+                                    
                                     (cond
-                                      ((= pivot -1)
-                                        (inverse-matrix-calculation mat invers i (+ j 1) n))
+                                      ((= pivot -1) ; If pivot not found, move to the next column
+                                        (inverse-matrix-calculation Mat Inverted_mat i (+ j 1) n))
                                       (( not (= i pivot))
                                        (let* (
-                                              (ri (.+ (sub invers i 0 1 n)(sub invers pivot 0 1 n)))
-                                              (mi  (insert-row invers i ri n))
-                                              (r (.+ (sub mat i 0 1 n)(sub mat pivot 0 1 n)))
-                                              (m  (insert-row mat i r n))
-                                              (coli (sub m 0 j n 1))
-                                              (tm1i (eliminate mi i j coli (ref m i j) n))
-                                              (tm1 (eliminate m i j coli (ref m i j) n)))
-                                         (inverse-matrix-calculation tm1 tm1i (+ i 1) (+ j 1) n)))
-                                      (else (let* ((tm2i (eliminate invers i j (sub mat 0 j n 1) (ref mat i j) n))
-                                                   (tm2 (eliminate mat i j (sub mat 0 j n 1) (ref mat i j) n)))
-                                              (inverse-matrix-calculation tm2 tm2i (+ i 1) (+ j 1) n)))) ))))
+                                              (row_sum_i (.+ (sub Inverted_mat i 0 1 n)(sub Inverted_mat pivot 0 1 n)))
+                                              (Mi  (insert-row Inverted_mat i row_sum_i n))
+                                              (row_sum (.+ (sub Mat i 0 1 n)(sub Mat pivot 0 1 n)))
+                                              (M_tmp  (insert-row Mat i row_sum n))
+                                              (V-col (sub M_tmp 0 j n 1))
+                                              (M_inv (eliminate Mi i j V-col (ref M_tmp i j) n))
+                                              (M (eliminate M_tmp i j V-col (ref M_tmp i j) n)))
+                                         (inverse-matrix-calculation M M_inv (+ i 1) (+ j 1) n)))
+                                      (else ; If current row is pivot row
+                                       (let* ((M_inv (eliminate Inverted_mat i j (sub Mat 0 j n 1) (ref Mat i j) n))
+                                                   (M (eliminate Mat i j (sub Mat 0 j n 1) (ref Mat i j) n)))
+                                              (inverse-matrix-calculation M M_inv (+ i 1) (+ j 1) n)))) ))))
                                        
                                        
-(define gaussian-elimination (λ (mat i j n)
-                             (if (or (= i n) (= j n)) mat
-                                 (let* ((pivot (find-pivot mat i j n)))
+(define gaussian-elimination (λ (Mat i j n rank-count det flag)
+                               ; Perform Gaussian elimination on a matrix to compute its rank and determinant
+                             (if (or (= i n) (= j n))
+                                 (if(zero? flag)
+                                  rank-count; Return rank if flag is 0
+                                                       det); Return determinant if flag is 1
+                                 (let* ((pivot (find-pivot Mat i j n)))
                                   (cond
-                                      ((= pivot -1)
-                                        (gaussian-elimination mat  i (+ j 1) n))
-                                      (( not (= i pivot))
+                                      ((= pivot -1); If pivot not found, move to the next column
+                                        (gaussian-elimination Mat  i (+ j 1) n rank-count det flag))
+                                      (( not (= i pivot)) ; If current row is not pivot row
                                         (let* (
-                                              (row-sum (.+ (sub mat i 0 1 n)(sub mat pivot 0 1 n)))
-                                              (m  (insert-row mat i row-sum n))
-                                              (m1 (eliminate-rows m i j (ref m i j) n))
+                                              (row-sum (.+ (sub Mat i 0 1 n)(sub Mat pivot 0 1 n)))
+                                              (M  (insert-row Mat i row-sum n))
+                                              (M-elim (eliminate-rows m i j (ref M i j) n))
                                               )
-                                          (gaussian-elimination m1  (+ i 1)  (+ j 1) n))
-                                 )
-                                      (else (let* (
-                                                   (m (eliminate-rows mat i j (ref mat i j) n)))
-                                              (gaussian-elimination m (+ i 1) (+ j 1) n))))
-                               )))
-                               )
-
-(define determinente-calculation (λ(mat det i n)
-                       
-                       (if (>= i n)  det  
-                       (determinente-calculation mat (* det (ref mat i i)) (+ i 1) n))))
-
-(define determinente (λ(mat)
-                      (let (
-                            (ranked (gaussian-elimination mat 0 0 (nrows mat))
-                                  ))(determinente-calculation ranked 1 0 (nrows mat) ))))
-(define scan-row(λ( mat row j n)
-               (cond(
-                     (= j n) 0)
-                     ((not(= (ref mat row j) 0)) 1)
-                  (scan-row mat row (+ j 1) n))))
-
-(define rank-calculation (λ(mat n i  r)
-               (let ((matrix(gaussian-elimination mat 0 0 n)))
-               (cond ((= i n) r)
-                   ((= (scan-row matrix i i n) 1)
-                    (rank-calculation matrix n (+ i 1) (+ r 1)))
-                    ))))
-
-(define rank (λ(mat)
-               (let(
-                    (ranked (gaussian-elimination mat 0 0 (nrows mat))  ))
-                 (rank-calculation ranked (nrows mat) 0 0))))
-
-(define inverse-matrix (λ(mat)
-                        (inverse-matrix-calculation mat (eye (nrows mat)) 0 0 (nrows mat))))
+                                          (gaussian-elimination M-elim  (+ i 1)  (+ j 1) n (+ rank-count 1) (* det (ref M-elim i j) flag ))) )
+                                      (else ; If current row is pivot row
+                                       (let* (
+                                                   (M (eliminate-rows Mat i j (ref Mat i j) n)))
+                                              (gaussian-elimination M (+ i 1) (+ j 1) n (+ rank-count 1) (* det (ref M i j)) flag )))) ))))
 
 
-(define A (matrix '((1 2 3) (4 5 6) (7 2 9))))
-(display A)
-(define I (eye 3))
-(define x (inverse-matrix A))
 
-(display "\ngaussian-elimination\n")
-(display x)
-(display "\n")
-(define r (rank A))
-(display "\n rank\n")
-(display r)
-(define d (determinente A))
-(display "\n determinente\n")
-(display d)
-(display (inv A)) 
+(define determinente (λ(Mat)
+                         ; Compute the determinant of a matrix using Gaussian elimination
+                       (gaussian-elimination Mat 0 0 (nrows Mat) 0 1 1 )))
 
+(define rank (λ(Mat)
+               ; Compute the rank of a matrix using Gaussian elimination
+           (gaussian-elimination Mat 0 0 (nrows Mat) 0 1 0)  ))
+               
+(define inverse-matrix (λ(Mat)
+                           ; Compute the inverted of a matrix using Gaussian elimination
+                        (inverse-matrix-calculation Mat (eye (nrows Mat)) 0 0 (nrows Mat))))
 
 ;read from a file  the matrix
 (define path_det "det_matrix(800 x 800).txt")
@@ -167,24 +128,27 @@
 (define m2 (matrix  (read_data path_rank delim)))
 (define t_start2 (current-inexact-milliseconds)) ;Get current time
 (define ra (rank  m2))
+(display "\n rank:  ")
 (display ra)
 (define t_end2 (current-inexact-milliseconds))
-(display "\n rank ")
+(display "\n rank time: ")
 (display (- t_end2 t_start2) )
 
 (define m (matrix  (read_data path_det delim)))
 (define t_start (current-inexact-milliseconds)) ;Get current time
 (define q (determinente  m))
+(display "\n det: ")
 (display q)
 (define t_end (current-inexact-milliseconds))
-(display "\n det ")
+(display "\n det time: ")
 (display (- t_end t_start) )
 
 (define m1 (matrix  (read_data path_inverse delim)))
 (define t_start1 (current-inexact-milliseconds)) ;Get current time
 (define i (inverse-matrix  m1))
+(display "\n inverse: ")
 (display i)
 (define t_end1 (current-inexact-milliseconds))
-(display "\n inverse ")
+(display "\n inverse time: ")
 (display (- t_end1 t_start1) )
 
